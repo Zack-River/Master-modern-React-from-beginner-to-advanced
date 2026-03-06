17eef13efbeee627ddf6761b7116017df6b03ccb
Zack River <elmutamiz@gmail.com>
2026-03-07 00:32:58 +0200

    section 06 - project 01: made rate component more reusable by passing many control props with default values by destructing for easier optional control without increasing mandatory complexity with the ability to export the state from the component

--- section-06-composition-and-reusablilty/01-use-popcorn/src/components/StarRating.js
+++ section-06-composition-and-reusablilty/01-use-popcorn/src/components/StarRating.js
@@ -10,19 +10,29 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-  color: "#000",
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
-  const [tempRating, setTempRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  fontSize = `${size / 1.5}px`,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize,
+  };
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(defaultRating);
 
   function handleRating(rating) {
     setRating(rating + 1);
     setTempRating(rating + 1);
+    onSetRating(rating + 1);
   }
 
   function handleTempRating(i) {
@@ -34,12 +44,14 @@ export default function StarRating({ maxRating = 5 }) {
   }
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => {
           return (
             <Star
               key={i}
+              color={color}
+              size={size}
               full={tempRating >= i + 1}
               onRate={() => handleRating(i)}
               onHoverIn={() => handleTempRating(i)}
@@ -48,19 +60,22 @@ export default function StarRating({ maxRating = 5 }) {
           );
         })}
       </div>
-      <p style={textStyle}>{tempRating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating - 1]
+          : tempRating || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: "2rem",
-  height: "2rem",
-  display: "block",
-  cursor: "pointer",
-};
-
-function Star({ onRate, onHoverIn, onHoverOut, full }) {
+function Star({ onRate, onHoverIn, onHoverOut, full, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
   return (
     <span
       role="button"
@@ -73,8 +88,8 @@ function Star({ onRate, onHoverIn, onHoverOut, full }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -83,7 +98,7 @@ function Star({ onRate, onHoverIn, onHoverOut, full }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- section-06-composition-and-reusablilty/01-use-popcorn/src/index.js
+++ section-06-composition-and-reusablilty/01-use-popcorn/src/index.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App'
 // import './index.css'
 import StarRating from "./components/StarRating";
+
+function Test() {
+  const [MovieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />
+      <p>
+        This Movie was
+        {MovieRating ? ` rated with ${MovieRating} starts` : " not rated yet"}
+      </p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      fontSize="2rem"
+      messages={["Terrible", "Bad", "Ok", "Good", "Great"]}
+      defaultRating={3}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color={"red"}
+      className="test"
+      defaultRating={2}
+    />
+    <Test />
   </React.StrictMode>,
 );
